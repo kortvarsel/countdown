e86f06d5ca1613b2f76df64e9f4ff6c0088efa35
Patric Hjorth <Patric@MacBook-Pro-3.local>
2024-05-11 11:34:29 +0200

    Added material design input components

--- src/countdown/countdown.component.ts
+++ src/countdown/countdown.component.ts
@@ -4,15 +4,28 @@ import { StringInputComponent } from "../string-input/string-input.component";
 import { TimerComponent } from "../timer/timer.component";
 import { FormsModule } from "@angular/forms";
 import { FitTextComponent } from "../fit-text/fit-text.component";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from "@angular/material/core";
 
 @Component({
     selector: "app-countdown",
     standalone: true,
     templateUrl: "./countdown.component.html",
-    imports: [TimerComponent, DateInputComponent, StringInputComponent, FormsModule, FitTextComponent],
+    providers: [
+        MatDatepickerModule,
+    ],
+    imports: [TimerComponent, DateInputComponent, StringInputComponent, FormsModule, FitTextComponent, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
     styleUrls: ["./countdown.component.scss"],
 })
+
 export class CountdownComponent {
+    constructor() {
+        this.loadTitleFromLocalStorage();
+        this.loadDateFromLocalStorage();
+    }
+
     private _title = '';
 
     get title(): string {
@@ -35,11 +48,6 @@ export class CountdownComponent {
         this.saveDateToLocalStorage(value);
     }
 
-    constructor() {
-        this.loadTitleFromLocalStorage();
-        this.loadDateFromLocalStorage();
-    }
-
     private saveTitleToLocalStorage(title: string) {
         localStorage.setItem('title', title);
     }
@@ -54,7 +62,6 @@ export class CountdownComponent {
             this._title = storedTitle;
         }
     }
-
 
     private loadDateFromLocalStorage(): void {
         const storedDate = localStorage.getItem('date');
