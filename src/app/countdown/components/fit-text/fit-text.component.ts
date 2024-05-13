d998dc6da945c7b4c014a4724e85eb076e50725f
Patric Hjorth <Patric@MacBook-Pro-3.local>
2024-05-13 11:54:20 +0200

    Introduced variables in fittext for padding & max/min font size

--- src/app/countdown/components/fit-text/fit-text.component.ts
+++ src/app/countdown/components/fit-text/fit-text.component.ts
@@ -11,9 +11,12 @@ import { Component, ElementRef, ViewChild, AfterViewInit, Input, SimpleChanges }
 export class FitTextComponent implements AfterViewInit {
     @ViewChild('fitTextContainer') container!: ElementRef<HTMLDivElement>;
 
-    private _fontSize = 16;
+    private _fontSize: number = 16;
+    minFontSize: number = 1;
+    maxFontSize: number = 10000;
     private _windowWidth: number = 0;
     private _contentWidth: number = 0;
+    padding: number = 16;
 
     //dependencies type is to avoid 'any' and can be expanded, as of now it only listens to changes to trigger fitText
     @Input() dependencies: (string | number)[] = [];
@@ -34,11 +37,11 @@ export class FitTextComponent implements AfterViewInit {
         if (!this.container) return;
         this._windowWidth = Math.min(window.innerWidth, window.outerWidth);
         this._contentWidth = this.container.nativeElement.clientWidth;
-        while (this._contentWidth + 32 > this._windowWidth && this._fontSize > 1) {
+        while (this._contentWidth + (this.padding * 2) > this._windowWidth && this._fontSize > this.minFontSize) {
             this._fontSize -= 1;
             this.updateFontSize();
         }
-        while (this._windowWidth > this._contentWidth + 32 && this._fontSize < 10000) {
+        while (this._windowWidth > this._contentWidth + (this.padding * 2) && this._fontSize < this.maxFontSize) {
             this._fontSize += 1;
             this.updateFontSize();
         }
